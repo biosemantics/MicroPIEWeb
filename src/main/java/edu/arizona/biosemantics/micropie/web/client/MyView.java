62c43a12a5da567a85755d9e68dc5aeb9596137f
huangfengq@gmail.com <new82884717>
2014-11-21 14:00:28 -0700

    Update on Nov 21, 2014

--- src/main/java/edu/arizona/biosemantics/micropie/web/client/MyView.java
+++ src/main/java/edu/arizona/biosemantics/micropie/web/client/MyView.java
@@ -2,7 +2,9 @@ package edu.arizona.biosemantics.micropie.web.client;
 
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.sencha.gxt.widget.core.client.container.SimpleContainer;
 
 import edu.arizona.biosemantics.micropie.web.shared.model.Something;
@@ -13,24 +15,28 @@ public class MyView extends SimpleContainer {
 
 	private IMyServiceAsync service = GWT.create(IMyService.class);
 	private Label text = new Label();
-	
+
 	public MyView() {
 
 		this.add(text);
-		
+
 		service.doSomething(new Something(), new AsyncCallback<String>() {
 			@Override
 			public void onFailure(Throwable t) {
 				t.printStackTrace();
 				text.setText("Something went wrong");
 			}
+
 			@Override
 			public void onSuccess(String result) {
 				text.setText("Hello World: " + result);
-			} 
+
+				// HTML html = new HTML("<p>" + text + "</p>");
+				// fRootPanel.get("micropieweb").add(html);
+
+			}
 		});
-		
-		
+
 	}
-	
+
 }
